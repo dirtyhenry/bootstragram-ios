c7f945df53c7ab3463e6ab56f24b001fc09c714c
Mickaël Floc'hlay <dirtyhenry@gmail.com>
2014-02-25 14:03:22 +0100

    Funky views

--- bootstragram/BSCoreGraphicsPlaygroundViewController.m
+++ bootstragram/BSCoreGraphicsPlaygroundViewController.m
@@ -18,8 +18,19 @@
 @implementation BSCoreGraphicsPlaygroundViewController
 
 
-- (void)viewDidLayoutSubviews {
-    self.funkyView.backgroundColor = [UIColor clearColor];
+- (void)viewWillAppear:(BOOL)animated {
+    CGRect screen = self.view.bounds;
+    CGFloat border = 40.0;
+    
+    BSFunkyView *view = [[BSFunkyView alloc] initWithFrame:CGRectMake(border, border, screen.size.width - 2 * border, screen.size.height - 2 * border)];
+    view.backgroundColor = [UIColor clearColor];
+    view.borderRadius = 40.0;
+    view.borderVerticalAngle = (15.0 / 180.0 * M_PI);
+    view.borderHorizontalAngle = (20.0 / 180.0 * M_PI);
+    view.borderVertical = 20.0;
+    view.borderHorizontal = 60.0;
+    
+    [self.view addSubview:view];
 }
 
 
